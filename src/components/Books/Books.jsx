392bcd69ebfc73a6cc98f37e8c3be97dbc4741e1
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-10-09 15:38:08 +0600

    set the each card on the books route

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Book from '../Book/Book';
 
 const Books = () => {
+
+    const {books}= useLoaderData();
+    console.log(books)
     return (
-        <div>
-            <h2>All Books are here </h2>
+        <div className='my-container'>
+
+            <div className='grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2'>
+
+                {
+                    books.map(book=> <Book  
+                      key={book.isbn13}
+                      book={book}
+                    >
+
+                    </Book>  )
+                }
+
+            </div>
+            
         </div>
     );
 };
